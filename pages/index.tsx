a062bd6bdcb8e144e4200ea924394d45191ec2b5
Bradley Freedman <bradleytfreedman@gmail.com>
2025-02-05 00:01:40 -0500

    Improved list view

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import DefaultLayout from "@/layouts/default";
 import {Input} from "@heroui/input"
 import {Divider} from "@heroui/divider";
-import {Button} from "@heroui/button"
+import {Button, ButtonGroup} from "@heroui/button"
 import { SearchIcon } from "../components/icons";
 import { useState } from "react";
 import {Listbox, ListboxItem} from "@heroui/listbox"
 import { Proposal, ProposedRuleChange } from "../types";
 import {Progress} from "@heroui/progress"
 import { ThemeSwitch } from "@/components/theme-switch";
+import { Switch } from "@heroui/switch";
+import { Spinner } from "@heroui/spinner"
 
 export async function getServerSideProps(ctx: any) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/proposals`);
@@ -24,6 +26,10 @@ export default function IndexPage(props: {firstProposalsPage: any}) {
   const [selectedProposal, setSelectedProposal] = useState<Proposal | "loading" | null>(null);
   const [proposedRuleChanges, setProposedRuleChanges] = useState<ProposedRuleChange[]>(props.firstProposalsPage.proposals)
   const [selectedProposalTitle, setSelectedProposalTitle] = useState<string | null>(null)
+  const [titleViewEnabled, setTitleViewEnabled] = useState<boolean>(true);
+  const [proposalPageLoading, setProposalPageLoading] = useState<boolean>(false);
+
+  let lastPageLoaded: number = 1;
 
   async function fetchProposal(id: string, title: string) {
     setSelectedProposal("loading")
@@ -33,24 +39,46 @@ export default function IndexPage(props: {firstProposalsPage: any}) {
     setSelectedProposal(proposal)
   }
   
+  function cleanTitle(title: string) {
+    return title.replace(/Federal Acquisition Regulations?:[:;]?\s?|FAR Case \d{4}-\d{3}[,;]?\s?/g, '').trim();
+  }
+
+  async function fetchProposals(page: number) {
+    const proposals = fetch(`/api/proposals?page=${page}`)
+  }
+  
   return (
     <DefaultLayout>
       <div className="grid grid-cols-[auto_auto_1fr] h-full pb-[64px]">
         <div id="search-column" className="flex flex-col h-full w-fit mr-5">
-          <div className="flex mb-5">
+          <div className="flex mb-5 px-[12px]">
             <Input className="mr-2 " placeholder="Search"/>
             <Button isIconOnly>
               <SearchIcon/>
             </Button>
           </div>
-          <Listbox>
-            {
-              proposedRuleChanges.map((proposal: ProposedRuleChange) => 
-                <ListboxItem className="max-w-[262px]" onPress={(e) => fetchProposal(proposal.id, proposal.attributes.title)}>
-                  {proposal.attributes.title}
-                </ListboxItem>
-              )
-            }
+          <div className="flex !justify-start !items-center px-[12px] mb-5">
+            <span className="mr-5">View Proposal Titles</span>
+            <Switch defaultSelected onValueChange={(value) => setTitleViewEnabled(value)}/>
+          </div>
+          <Listbox >
+            <>
+              {
+                proposedRuleChanges.map((proposal: ProposedRuleChange) => 
+                  <ListboxItem className="max-w-[262px]" onPress={(e) => fetchProposal(proposal.id, proposal.attributes.title)}>
+                    {
+                      titleViewEnabled ? cleanTitle(proposal.attributes.title) : proposal.id
+                    }
+                  </ListboxItem>
+                  
+                )
+              }
+              {
+                proposalPageLoading ?
+                <ListboxItem isReadOnly><i>Load More</i></ListboxItem> :      
+                <ListboxItem isDisabled><Spinner/></ListboxItem>
+              }
+            </>     
           </Listbox>
         </div>
         <div>
@@ -59,8 +87,8 @@ export default function IndexPage(props: {firstProposalsPage: any}) {
         <div>
           {
             !selectedProposal &&
-            <div className="w-full h-full flex justify-center items-center">
-              dfsgdf
+            <div className="w-full h-full flex justify-center items-center opacity-50">
+              Select a Proposal
             </div>
           }
           {
