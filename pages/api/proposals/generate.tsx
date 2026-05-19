f8069481611401d6d931a0ddb9009f8a92f618b4
Nathan Self <nwself@vt.edu>
2026-05-19 11:10:13 -0400

    Remove nextjs project

--- pages/api/proposals/generate.tsx
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import { generateProposal } from "@/lib/llm";
-
-export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
-  const proposal = await generateProposal(req.body.NDAASection, req.body.year, req.body.FARSection);
-  res.status(200).json(proposal);
-}
--- pages/api/proposals/summarizeComments.tsx
+++
@@ -1,8 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-import { summarizeOverall } from "@/lib/llm";
-
-export default async function Handler(req: NextApiRequest, res: NextApiResponse) {
-  const summary = await summarizeOverall(req.body.summaries);
-  res.status(200).json(summary);
-}
